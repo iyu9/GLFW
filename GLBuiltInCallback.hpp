c1b1b838bdb9a6c9be6770ec0e815b06d3cf3a6a
iyu9 <tomii.yuto@gmail.com>
2017-11-29 16:17:16 +0900

    Update GLBuiltInCallback.hpp

--- GLBuiltInCallback.hpp
+++ GLBuiltInCallback.hpp
@@ -5,7 +5,51 @@ extern GLVec2 pos;
 
 static void error_callback(int error, const char* description)
 {
-  std::cout << description;
+  std::cout << "error_description = " << description << std::endl;
+  std::cout << "error_code = " << error << std::endl;
+
+  switch (error)
+  {
+  case GLFW_NOT_INITIALIZED:
+    std::cout << "GLFW_NOT_INITIALIZED";
+    break;
+
+  case GLFW_NO_CURRENT_CONTEXT:
+    std::cout << "GLFW_NO_CURRENT_CONTEXT";
+    break;
+
+  case GLFW_INVALID_ENUM:
+    std::cout << "GLFW_INVALID_ENUM";
+    break;
+
+  case GLFW_INVALID_VALUE:
+    std::cout << "GLFW_INVALID_VALUE";
+    break;
+
+  case GLFW_OUT_OF_MEMORY:
+    std::cout << "GLFW_OUT_OF_MEMORY";
+    break;
+
+  case GLFW_API_UNAVAILABLE:
+    std::cout << "GLFW_API_UNAVAILABLE";
+    break;
+
+  case GLFW_VERSION_UNAVAILABLE:
+    std::cout << "GLFW_VERSION_UNAVAILABLE";
+    break;
+
+  case GLFW_PLATFORM_ERROR:
+    std::cout << "GLFW_PLATFORM_ERROR";
+    break;
+
+  case GLFW_FORMAT_UNAVAILABLE:
+    std::cout << "GLFW_FORMAT_UNAVAILABLE";
+    break;
+  }
+
+  //Wait
+  std::string inputStr;
+  std::cin >> inputStr;  
 }
 
 static void cursor_pos_callback(GLFWwindow* window, double x, double y)
